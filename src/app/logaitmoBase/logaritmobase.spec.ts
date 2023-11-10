e8a2e69747bc3147e608caafe0ed9743955a5ba3
Joseemmanuel <emmanuelriv15@gmail.com>
2023-11-10 12:24:49 -0600

    Update logaritmoBase branch #4

--- src/app/logaitmoBase/logaritmobase.spec.ts
+++ src/app/logaitmoBase/logaritmobase.spec.ts
@@ -1,8 +1,36 @@
-export function logaritmoBase10(n: number): number {
-  if (n <= 0) {
-    throw new Error(
-      "El logaritmo en base 10 solo está definido para números positivos."
-    );
-  }
-  return Math.log10(n);
-}
+import { logaritmoBase10 } from "./logaritmobase";
+
+describe("Tests for the base 10 logarithm function", () => {
+  it("Should calculate the base 10 logarithm of 10 as 1", () => {
+    // Arrange
+    let result = 0;
+
+    // Act
+    result = logaritmoBase10(10);
+
+    // Assert
+    expect(result).toBe(1);
+  });
+
+  it("Should calculate the base 10 logarithm of 100 as 2", () => {
+    // Arrange
+    let result = 0;
+
+    // Act
+    result = logaritmoBase10(100);
+
+    // Assert
+    expect(result).toBe(2);
+  });
+
+  it("Should calculate the base 10 logarithm of 1000 as 3", () => {
+    // Arrange
+    let result = 0;
+
+    // Act
+    result = logaritmoBase10(1000);
+
+    // Assert
+    expect(result).toBe(3);
+  });
+});
